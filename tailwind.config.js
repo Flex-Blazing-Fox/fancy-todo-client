1d88b94457cf4d0eb23e1effecd0d7a6f926ff8b
Rangga Putra Pertama <ranggarppb@MacBook-Pro-Rangga.local>
2021-07-02 19:51:15 +0700

    revise login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primarycolor: "#F9A826",
+      },
+    },
+    fontFamily: {
+      display: ["Nunito", "sans-serif"],
+    },
   },
   variants: {
     extend: {},
